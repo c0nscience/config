b8031d261e810a229adcef3f7a49e4dd57623b86
Benjamin Herzig <benjamin.herzig@nist.de>
2019-06-13 16:03:20 +0200

    initial commit

---
+++ alias.sh
@@ -0,0 +1,5 @@
+alias zshconfig="code ~/.zshrc"
+alias ohmyzsh="code ~/.oh-my-zsh"
+alias baem="pwsh -c Finish-Release"
+alias dry-baem="pwsh -c 'Finish-Release -DryRun \$True'"
+alias refresh="git com && git pfo && git rmu"
