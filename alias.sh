8d5de2431f6759cd101d161868d79e3228d47677
Benjamin Herzig <benjamin.herzig@nist.de>
2020-03-12 17:20:46 +0100

    changes

--- alias.sh
+++ alias.sh
@@ -3,3 +3,6 @@ alias ohmyzsh="code ~/.oh-my-zsh"
 alias baem="pwsh -c Finish-Release"
 alias dry-baem="pwsh -c 'Finish-Release -DryRun \$True'"
 alias refresh="git com && git pfo && git rmu"
+
+alias sandbox="sfdx force:org:open -u mysb"
+alias production="sfdx force:org:open -u prod"
