215ffd881ee945764759855c2158ba47232f237f
Benjamin Herzig <ber.bherzig@pm.me>
2024-02-19 12:41:27 +0100

    clean up

--- alias.sh
+++
@@ -1,8 +0,0 @@
-alias zshconfig="code ~/.zshrc"
-alias ohmyzsh="code ~/.oh-my-zsh"
-alias baem="pwsh -c Finish-Release"
-alias dry-baem="pwsh -c 'Finish-Release -DryRun \$True'"
-alias refresh="git com && git pfo && git rmu"
-
-alias sandbox="sfdx force:org:open -u mysb"
-alias production="sfdx force:org:open -u prod"
