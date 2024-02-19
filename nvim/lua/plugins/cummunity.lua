58ce130e6669a06f188c57b64702de9f3c7c90cf
Benjamin Herzig <ber.bherzig@pm.me>
2024-02-19 23:02:29 +0100

    add nushell, nvim and windows terminal configs

---
+++ nvim/lua/plugins/cummunity.lua
@@ -0,0 +1,9 @@
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.docker" },
+  -- { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.tailwindcss" },
+}
